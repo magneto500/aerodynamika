d4df94502a3eed3ac00f8f0f55f0c8f0014e3485
Tomek <93818008+magneto500@users.noreply.github.com>
2022-02-09 18:35:44 +0100

    first commit

---
+++ script.js/hideKropelkaButton.js
@@ -0,0 +1,7 @@
+hideKropelkaButton.js
+let przycisk = document.querySelector(".przycisk");
+let zdjecie = document.querySelector(".zdjecie");
+przycisk.addEventListener("click", () => {
+    zdjecie.remove();
+});
+console.log("mail: tomasz.piotr.grabowski@gmail.com")
